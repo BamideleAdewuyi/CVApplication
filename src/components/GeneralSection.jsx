754b3e4c8f186fc45b0ff514007cb00c0394eaab
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-12 22:18:23 +0100

    GeneralSection.jsx: Added inputs for name, email and phone number

---
+++ src/components/GeneralSection.jsx
@@ -0,0 +1,14 @@
+import Input from "./Input";
+
+function GeneralSection() {
+    return (
+        <>
+        <h2>Personal Details</h2>
+        <Input label="Name"/>
+        <Input label="Email"/>
+        <Input label="Phone Number"/>
+        </>
+    )
+};
+
+export default GeneralSection;

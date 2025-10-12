1865a6e16e3d8064598171dd173f0b8b945cc933
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-12 21:27:39 +0100

    Input.jsx: Wrote and exported Input function

---
+++ src/components/Input.jsx
@@ -0,0 +1,20 @@
+function Input({ label }) {
+  const [text, setText] = useState('');
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={text}
+        onChange={handleChange}
+      />
+    </label>
+  );
+}
+
+export default Input;

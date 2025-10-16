20e99a1271ea65aedc9535ff3042b184d0f4e721
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-16 12:28:17 +0100

    CV.jsx: handleSubmit function now creates variables for name, email and phoneNumber

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,6 +4,9 @@ import './CV.css'
 function CV() {
     function handleSubmit(e) {
       e.preventDefault();
+      const name = e.target.elements.name.value;
+      const email = e.target.elements.email.value;
+      const phoneNumber = e.target.elements.phoneNumber.value;
    }
     return (
         <form action="" onSubmit={handleSubmit}>

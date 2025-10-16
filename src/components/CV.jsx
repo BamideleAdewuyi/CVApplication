227fd0b2fe5cf4e3ab2b638ab5890d16805292f9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-16 14:58:39 +0100

    CV.jsx: Set up isFormVisible useState. Submit button now sets it to true

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -3,13 +3,22 @@ import './CV.css'
 import { useState } from "react";
 
 function CV() {
+    const [isFormVisible, setFormVisible] = useState(false);
+
     function handleSubmit(e) {
       e.preventDefault();
       const name = e.target.elements.name.value;
       const email = e.target.elements.email.value;
       const phoneNumber = e.target.elements.phoneNumber.value;
+      setFormVisible(true);
    }
+   
     return (
+        isFormVisible ? 
+        <div>
+            CV appears here
+        </div>
+        :
         <form action="" onSubmit={handleSubmit}>
         <GeneralSection/>
         <button>Submit</button>

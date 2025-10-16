e01c6c8dd02487fd1a205c305bafa10d9bf33d1e
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-16 16:03:26 +0100

    CV.jsx: Swapped round isFormVisible returns. Initial state is now true

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -3,31 +3,31 @@ import './CV.css'
 import { useState } from "react";
 
 function CV() {
-    const [isFormVisible, setFormVisible] = useState(false);
+    const [isFormVisible, setFormVisible] = useState(true);
 
     function handleSubmit(e) {
       e.preventDefault();
       const name = e.target.elements.name.value;
       const email = e.target.elements.email.value;
       const phoneNumber = e.target.elements.phoneNumber.value;
-      setFormVisible(true);
+      setFormVisible(false);
    }
 
    function editForm() {
-    setFormVisible(false);
+    setFormVisible(true);
    }
    
     return (
         isFormVisible ? 
-        <div>
-            CV appears here
-            <button onClick={editForm}>Edit</button>
-        </div>
-        :
         <form action="" onSubmit={handleSubmit}>
         <GeneralSection/>
         <button>Submit</button>
         </form>
+        :
+        <div>
+            CV appears here
+            <button onClick={editForm}>Edit</button>
+        </div>
     )
 }
 

eec26f8cc031bd91cd37807886ffcdd1bf306d72
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-18 17:46:15 +0100

    CV.jsx: name, email and phoneNumber are now state variables that are updated on submit

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,12 +4,18 @@ import { useState } from "react";
 
 function CV() {
     const [isFormVisible, setFormVisible] = useState(true);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
 
     function handleSubmit(e) {
       e.preventDefault();
       const name = e.target.elements.name.value;
       const email = e.target.elements.email.value;
       const phoneNumber = e.target.elements.phoneNumber.value;
+      setName(e.target.elements.name.value);
+      setEmail(e.target.elements.email.value);
+      setPhoneNumber(e.target.elements.phoneNumber.value);
       setFormVisible(false);
    }
 

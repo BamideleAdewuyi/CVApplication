d510eb474cb0779f126c4d7b9244adc3f49757d5
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-18 19:23:13 +0100

    Submissions.jsx: Wrote part of Submissions function and exported

---
+++ src/components/Submissions.jsx
@@ -0,0 +1,11 @@
+function Submissions(name, email, phoneNumber) {
+    return(
+        <>
+        <h3>{name}</h3>
+        <h4>{email}</h4>
+        <h5>{phoneNumber}</h5>
+        </>
+    )
+}
+
+export default Submissions;

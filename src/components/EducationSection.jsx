00965d63f1138e529ffe281d3d39148b79b5d0b8
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-19 17:54:57 +0100

    EducationSection.jsx: Wrote EudcationSection function

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,7 +1,26 @@
 import Input from "./Input";
 
-function EducationSection() {
-
+function EducationSection({school, subject, year}) {
+    return(
+        <>
+        <Input 
+        label="School/University" 
+        className="educationSectionInput"
+        name="school"
+        value={school}/>
+        <Input 
+        label="Subject" 
+        className="educationSectionInput"
+        name="subject"
+        value={subject}/>
+        <Input 
+        label="Year completed" 
+        className="educationSectionInput"
+        name="year"
+        type="month"
+        value={year}/>
+        </>
+    )
 };
 
 export default EducationSection;

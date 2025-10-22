62d58851fb50d4ac35134f487d215337c69c0a29
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-22 22:06:30 +0100

    Submissions.jsx: Added headings to personal details and education sections

--- src/components/Submissions.jsx
+++ src/components/Submissions.jsx
@@ -1,9 +1,11 @@
 function Submissions({name, email, phoneNumber}) {
     return(
         <>
+        <h2>Personal Details</h2>
         <h3>{name}</h3>
         <h4>{email}</h4>
         <h5>{phoneNumber}</h5>
+        <h2>Education</h2>
         </>
     )
 }

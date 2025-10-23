33f64e46c3c5074ecba54a593d84fcf016029f3f
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-23 17:02:47 +0100

    CV.jsx: Delete buttons call deleteSchool on click

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -68,7 +68,7 @@ function CV() {
                 school= {education[index] ? education[index].school : ""}
                 subject={education[index] ? education[index].subject : ""}
                 year={education[index] ? education[index].year : ""}/>
-                <button type="button" key={`educationButton${index}`}>Delete</button>
+                <button type="button" onClick={deleteSchool} key={`educationButton${index}`}>Delete</button>
             </div>
 
         ))}

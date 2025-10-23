a6f6b8dd4f2568f93b6c293970f8d568c74eb298
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-10-23 01:28:33 +0100

    Submissions.jsx: Added array to fill out education submissions

--- src/components/Submissions.jsx
+++ src/components/Submissions.jsx
@@ -1,4 +1,4 @@
-function Submissions({name, email, phoneNumber}) {
+function Submissions({name, email, phoneNumber, education}) {
     return(
         <>
         <h2>Personal Details</h2>
@@ -6,6 +6,13 @@ function Submissions({name, email, phoneNumber}) {
         <h4>{email}</h4>
         <h5>{phoneNumber}</h5>
         <h2>Education</h2>
+        {Array.from({length: education.length}).map((_, index) => (
+            <div className="educationSubmissions" key={`educationSubmissions${index}`}>
+                <p>School/University: {education[index].school}</p>
+                <p>Subject: {education[index].subject}</p>
+                <p>Completed: {education[index].year}</p>
+            </div>
+        ))}
         </>
     )
 }
